0f6f93ea064e156c352ee1927e546c5f62dece0c
Saymon <kroxotylik@gmail.com>
2022-01-05 19:40:51 +0300

    seven step (front not working) label work in postman good

--- src/main/java/hexlet/code/app/model/Label.java
+++ src/main/java/hexlet/code/app/model/Label.java
@@ -6,19 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
-import java.util.List;
 
 import static javax.persistence.TemporalType.TIMESTAMP;
 
@@ -37,9 +33,6 @@ public class Label {
     @NotEmpty(message = "Label name field must not be empty")
     @Size(min = 1)
     private String name;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    private List<Task> task;
 
     @CreationTimestamp
     @Temporal(TIMESTAMP)
