9552fdf1611c77cf98d0c87d470c65b406615a89
Saymon <kroxotylik@gmail.com>
2022-01-18 20:43:15 +0300

    del frontend

--- frontend/src/contexts/index.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import { createContext } from 'react';
-
-export const AuthContext = createContext({});
-
-export const ApiContext = createContext(null);
-
-export const NotificationContext = createContext({ addMessages: () => {}, messages: [] });
