de6e718b2f43c35dfb02e27ddd9f31e70827cc33
Saymon <kroxotylik@gmail.com>
2022-01-20 13:07:52 +0300

    edit jwtFilter(replaceAll)

--- src/main/java/hexlet/code/config/security/JwtTokenFilter.java
+++ src/main/java/hexlet/code/config/security/JwtTokenFilter.java
@@ -41,7 +41,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             return;
         }
 
-        final String token = header.split(" ")[1].trim();
+        final String token = header.replaceAll("^Bearer", "").trim();
+//        final String token = header.split(" ")[1].trim();
         if (!jwtTokenUtil.validate(token)) {
             chain.doFilter(request, response);
             return;
